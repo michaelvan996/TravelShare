ecc1a109394216efd119141788812d3dcf752f42
Michael Van <michaelvan996@gmail.com>
2023-07-24 18:52:07 -0400

    completed routing, backend folder, controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import travelShare from './images/travelShare.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Travel Share</Typography>
+                <img src={travelShare} alt="travel share" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
